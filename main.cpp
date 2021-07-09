f96947f5a96213b1730297ad545b9f9addec250c
BuyankinaYana <yana.buyanckina@yandex.ru>
2021-07-09 21:03:17 +0300

    Исправленная программа 4 вариант (сортировка по алфавиту)

--- main.cpp
+++ main.cpp
@@ -6,57 +6,58 @@ using namespace std;
 
 int main()
 {
-    string a;
-    string x;
+    string line;
+    string Input_file_name;
     string c;
-    string h;
-    vector<string> v;
+    string Output_file_name;
+    vector<string> array_of_strings;
 
-    //запрос имен файлов
+    //запрос пути к считываемому файлу
     cout<<"Enter path to file"<<endl;
-    cin>>x;
+    cin>>Input_file_name;
+    //запрос пути к записываемому файлу
     cout<<"Enter the address where you want to save the data"<<endl;
-    cin>>h;
+    cin>>Output_file_name;
 
     //чтение из файла
     ifstream in;
-    in.open(x);
-    while (!in.eof())
+    in.open(Input_file_name);
+    while (getline(in,line))
     {
-        getline(in,a);
-        cout<<a<<endl;
-        v.push_back(a);
+        cout<<line<<endl;
+        array_of_strings.push_back(line);
     }
     in.close();
 
     //обработка методом пузырька
-    for (int i=0; i<v.size()-1; i++)
+    for (int i=0; i<array_of_strings.size()-1; i++)
     {
-        for (int j=i+1; j<v.size(); j++)
+        for (int j=i+1; j<array_of_strings.size(); j++)
         {
-            if (v.at(i)>v.at(j))
+            if (array_of_strings.at(i)>array_of_strings.at(j))
             {
-                c=v.at(i);
-                v.at(i)=v.at(j);
-                v.at(j)=c;
+                c=array_of_strings.at(i);
+                array_of_strings.at(i)=array_of_strings.at(j);
+                array_of_strings.at(j)=c;
             }
         }
     }
 
     //вывод результата на экран
+    cout<<" "<<endl;
     cout<<"Result"<<endl;
     cout<<" "<<endl;
-    for (int i=0; i<v.size(); i++)
+    for (int i=0; i<array_of_strings.size(); i++)
     {
-        cout<<v.at(i)<<endl;
+        cout<<array_of_strings.at(i)<<endl;
     }
 
     //запись файла
     ofstream out;
-    out.open(h);
-    for (int i=0; i<v.size(); i++)
+    out.open(Output_file_name);
+    for (int i=0; i<array_of_strings.size(); i++)
     {
-        out<<v.at(i)<<endl;
+        out<<array_of_strings.at(i)<<endl;
     }
     out.close();
     return 0;
